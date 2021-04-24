6a46be65df4efca5082e8389cf663dcedd34d1f3
krutibaraiya <baraiyakruti@gmail.com>
2021-04-24 12:19:56 +0530

    Gitignore modified

--- CMakeFiles/CurvedShadows.dir/Parser.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/CurvedShadows.dir/Parser.cpp.o: \
- /home/kruti/Desktop/Sristi_ISF311/CurvedShadows/Parser.cpp \
- /usr/include/stdc-predef.h
--- CMakeFiles/CurvedShadows.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for CurvedShadows.
-# This may be replaced when dependencies are built.
